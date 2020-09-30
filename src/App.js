c1c9329355f6730a429ea2390c748a1690bf63da
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-09-30 11:34:09 -0600

    Changes to correctly show data and further connect front/backend.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			shoes: [],
+		};
+	}
+
+	componentDidMount() {
+		fetch("http://localhost:8000/api/shoe/")
+			.then((res) => res.json())
+			.then((data) => this.setState({ shoes: data }));
+	}
+	render() {
+		return (
+			<div>
+				<h2>
+					<u>Shoes currently available:</u>
+				</h2>
+				<ul>
+					{this.state.shoes.map((s) => (
+						<ul>
+							<li>Size: {s.size}</li>
+							<li>Brand Name: {s.brand_name}</li>
+							<li>Manufacturer: {s.manufacturer}</li>
+							<li>Color: {s.color}</li>
+							<li>Material: {s.material}</li>
+							<li>Shoe Type: {s.shoe_type}</li>
+							<li>Fasten Type: {s.fasten_type}</li>
+							<br />
+						</ul>
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
